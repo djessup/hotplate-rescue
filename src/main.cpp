056fdbfa66ba3f58b7867860ba77200707f42761
djessup <djessup@users.noreply.github.com>
2023-12-10 18:16:28 +1100

    Basically functional now

--- src/main.cpp
+++ src/main.cpp
@@ -11,67 +11,48 @@ using namespace ace_button;
 #define PIN_TEMP PIN_A0
 #define PIN_ISENSE PIN_A1
 #define PIN_HEATER 3
-#define PIN_BTN_UP 4
+#define PIN_BTN_UP 10
 #define PIN_BTN_OK 7
 #define PIN_BTN_DOWN 8
 
-#define DEBOUNCE_DELAY 200 // in ms
+#define MIN_TEMP 20
+#define MAX_TEMP 220
+#define SAMPLE_DELAY 2
 
-// The beta coefficient of the thermistor (usually 3000-4000)
-#define BETA_COEFFICIENT 3950
-// temp. for nominal resistance (almost always 25 C)
-#define TEMPERATURENOMINAL 25
-// resistance at 25 degrees C
-#define THERMISTORNOMINAL 100000
-// series resistance (47k pull-up)
-#define SERIESRESISTOR 47000
+#define DEFAULT_TARGET_TEMP 160
+#define EEPROM_SETTINGS_ADDR 0
 
-#define EEPROM_SETTINGS 0;
+uint16_t lastRefresh = 0, refreshRate = 500;
 
 struct HeaterSettings {
     float targetTemp;
 };
 
-bool i2CAddrTest(uint8_t addr);
-//
-//volatile uint16_t interruptCnt_btnUp, interruptCnt_btnDown, interruptCnt_btnOk = 0;
-//
-//uint32_t last_interrupt_time_btnUp = 0;
-//
-//void isr_btnUpDebounce() {
-//    uint32_t this_interrupt_time = millis();
-//
-//    if (this_interrupt_time - last_interrupt_time_btnUp > DEBOUNCE_DELAY) {
-//        interruptCnt_btnUp++;
-//    }
-//
-//    last_interrupt_time_btnUp = this_interrupt_time;
-//}
-//
-//void isr_btnUp() {
-//    interruptCnt_btnUp++;
-//}
-//
-//void isr_btnDown() {
-//    interruptCnt_btnDown++;
-//}
-//
-//void isr_btnOk() {
-////    isr_btnUpDebounce();
-//    interruptCnt_btnOk++;
-//}
+HeaterSettings currentSettings{
+        .targetTemp = DEFAULT_TARGET_TEMP,
+};
 
-void storeSettings(HeaterSettings settings) {
-    int eeAddress = EEPROM_SETTINGS;
-    EEPROM.put(eeAddress, settings);
+AceButton btn_Up(PIN_BTN_UP);
+AceButton btn_Down(PIN_BTN_DOWN);
+AceButton btn_Ok(PIN_BTN_OK);
+
+bool led, heaterEnable, saveSettings = false;
+
+float lastTemp = 0;
+
+bool i2CAddrTest(uint8_t addr);
+
+bool check_refresh();
+
+void storeSettings() {
+    int eeAddress = EEPROM_SETTINGS_ADDR;
+    EEPROM.put(eeAddress, currentSettings);
+    saveSettings = false;
     Serial.println("Settings saved.");
 }
 
-
-#define DEFAULT_TARGET_TEMP 160
-
 HeaterSettings loadSettings() {
-    int eeAddress = EEPROM_SETTINGS;
+    int eeAddress = EEPROM_SETTINGS_ADDR;
     HeaterSettings settings{};
     EEPROM.get(eeAddress, settings);
 
@@ -81,13 +62,6 @@ HeaterSettings loadSettings() {
     return settings;
 }
 
-HeaterSettings currentSettings{};
-
-AceButton btn_Up(PIN_BTN_UP);
-AceButton btn_Down(PIN_BTN_DOWN);
-AceButton btn_Ok(PIN_BTN_OK);
-
-void handleEvent(AceButton*, uint8_t, uint8_t);
 
 void setup() {
     delay(1000); // some microcontrollers reboot twice
@@ -127,13 +101,7 @@ void setup() {
     buttonConfig->setEventHandler(handleEvent);
     buttonConfig->setFeature(ButtonConfig::kFeatureClick);
     buttonConfig->setFeature(ButtonConfig::kFeatureDoubleClick);
-    buttonConfig->setFeature(ButtonConfig::kFeatureLongPress);
     buttonConfig->setFeature(ButtonConfig::kFeatureRepeatPress);
-
-    // Interrupts
-    // enableInterrupt(PIN_BTN_UP, isr_btnUpDebounce, FALLING);
-    // enableInterrupt(PIN_BTN_DOWN, isr_btnDown, FALLING);
-    // enableInterrupt(PIN_BTN_OK, isr_btnOk, FALLING);
 
     // Check if the button was pressed while booting
     if (btn_Up.isPressedRaw()) {
@@ -149,8 +117,6 @@ void setup() {
     Serial.println(F("setup(): ready"));
 }
 
-bool led, heaterEnable = false;
-
 
 float readTemp(uint8_t NUMSAMPLES = 5) {
 
@@ -158,7 +124,7 @@ float readTemp(uint8_t NUMSAMPLES = 5) {
     // take N samples in a row, with a slight delay
     for (int i = 0; NUMSAMPLES > i; i++) {
         average += analogRead(PIN_TEMP);
-        delay(10);
+        delay(SAMPLE_DELAY);
     }
 
     // average samples
@@ -182,79 +148,43 @@ float readTemp(uint8_t NUMSAMPLES = 5) {
     return Tc;
 }
 
-#define MIN_TEMP 20
-#define MAX_TEMP 220
 
-// Thermistor lookup table for RepRap Temperature Sensor Boards (http://make.rrrf.org/ts)
-// Made with createTemperatureLookup.py (http://svn.reprap.org/trunk/reprap/firmware/Arduino/utilities/createTemperatureLookup.py)
-// ./createTemperatureLookup.py --r0=100000 --t0=25 --r1=0 --r2=4700 --beta=3985 --max-adc=1023
-// r0: 100000
-// t0: 25
-// r1: 0
-// r2: 4700
-// beta: 3985
-// max adc: 1023
-#define NUMTEMPS 20
-float tempTable[NUMTEMPS][2] = {
-        {1,    906},
-        {54,   263},
-        {107,  215},
-        {160,  189},
-        {213,  171},
-        {266,  157},
-        {319,  145},
-        {372,  135},
-        {425,  126},
-        {478,  118},
-        {531,  110},
-        {584,  103},
-        {637,  95},
-        {690,  88},
-        {743,  80},
-        {796,  71},
-        {849,  62},
-        {902,  50},
-        {955,  34},
-        {1008, 2}
-};
-
-float targetTemp = 150;
-
-void toggle_led();
-
-void check_buttons();
-
-void update_state(float temp, bool heaterEnabled);
-
-void update_lcd(float temp, bool b);
-
-//float read_temp(int samples);
-
-void toggle_led();
-
-
-bool toggle_heater();
-
-void temp_decrease();
-
-void temp_increase();
 
 void loop() {
-
-    float temp = readTemp(5);
-
-    update_lcd(temp, heaterEnable);
-    toggle_led();
+    bool refresh = check_refresh();
 
     check_buttons();
 
-    update_state(temp, heaterEnable);
+    float temp = readTemp(5);
+    lastTemp = temp;
 
-    delay(250); // refresh rate
+    check_buttons(); // again, more responsive \__("o")__/
+
+    if (refresh) {
+        update_lcd(temp);
+        toggle_led();
+    }
+
+//    Serial.print("millis: "); Serial.println(millis());
+
+    update_state(temp);
+
+    if (saveSettings) {
+        storeSettings();
+    }
 }
 
-void update_state(float temp, bool heaterEnabled) {
-    if (temp < targetTemp && heaterEnabled) {
+bool check_refresh() {
+    long t = millis() / refreshRate;
+    if (t > lastRefresh) {
+        lastRefresh = t;
+        return true;
+    }
+    return false;
+}
+
+void update_state(float temp) {
+    if (temp < currentSettings.targetTemp && heaterEnable) {
         digitalWrite(PIN_HEATER, 1);
     } else {
         digitalWrite(PIN_HEATER, 0);
@@ -269,6 +199,7 @@ void check_buttons() {
 
 // The event handler for both buttons.
 void handleEvent(AceButton* button, uint8_t eventType, uint8_t buttonState) {
+
 
     // Print out a message for all events, for both buttons.
     Serial.print(F("handleEvent(): pin: "));
@@ -293,25 +224,30 @@ void handleEvent(AceButton* button, uint8_t eventType, uint8_t buttonState) {
             }
             break;
         case AceButton::kEventClicked:
+        case AceButton::kEventRepeatPressed:
             if (button->getPin() == PIN_BTN_UP) {
                 temp_increase();
             } else if (button->getPin() == PIN_BTN_DOWN) {
                 temp_decrease();
             }
+            update_lcd(lastTemp);
             break;
     }
 }
+
 
 void temp_increase() {
     if (currentSettings.targetTemp < MAX_TEMP) {
         currentSettings.targetTemp++;
     }
+    saveSettings = true;
 }
 
 void temp_decrease() {
     if (currentSettings.targetTemp > MIN_TEMP) {
         currentSettings.targetTemp--;
     }
+    saveSettings = true;
 }
 
 // toggle heater state and return the new state
@@ -334,50 +270,15 @@ void toggle_led() {
     digitalWrite(LED_BUILTIN, led);
 }
 
-//@deprecated
-//float read_temp(int samples = 1) {
-//    int i;
-//
-//    float rawTemp = 0;
-//    for (i = 0; i < samples; i++) {
-//        rawTemp += analogRead(PIN_TEMP);
-//    }
-//    rawTemp /= samples;
-//
-//    float celsius = 0;
-//
-//    for (i = 1; i < NUMTEMPS; i++) {
-//        if (tempTable[i][0] > rawTemp) {
-//            float realTemp = tempTable[i - 1][1]
-//                             + (rawTemp - tempTable[i - 1][0])
-//                               * (tempTable[i][1] - tempTable[i - 1][1])
-//                               / (tempTable[i][0] - tempTable[i - 1][0]);
-//
-//            if (realTemp > 255) {
-//                realTemp = 255;
-//            }
-//
-//            celsius = realTemp;
-//
-//            break;
-//        }
-//    }
-//
-//    // Overflow: We just clamp to 0 degrees celsius
-//    if (i == NUMTEMPS)
-//        celsius = 0;
-//
-//    return celsius;
-//}
 
-void update_lcd(float temp, bool heaterEnabled) {
+void update_lcd(float temp) {
     lcd.setCursor(0, 0);  // set the cursor to column 0, line 0
     lcd.print("T: ");
     lcd.print(temp);
     lcd.print("/");
-    lcd.print(round(targetTemp));
+    lcd.print(round(currentSettings.targetTemp));
 
     lcd.setCursor(0, 1);
     lcd.print("Heater: ");
-    lcd.print(((heaterEnabled) ? "ARMED" : "Safe "));
+    lcd.print(((heaterEnable) ? "ARMED" : "Safe "));
 }
--- src/main.h
+++ src/main.h
@@ -3,10 +3,31 @@
 
 #include "Arduino.h"
 #include "LiquidCrystal_I2C.h"
+#include "AceButton.h"
+
+using namespace ace_button;
 
 #define DISPLAY_ROWS 16
 #define DISPLAY_COLS 2
 #define DISPLAY_ADDR 0x27
+
+
+
+void handleEvent(AceButton* button, uint8_t eventType, uint8_t buttonState);
+
+void check_buttons();
+
+void update_state(float temp);
+
+void update_lcd(float temp);
+
+void toggle_led();
+
+bool toggle_heater();
+
+void temp_decrease();
+
+void temp_increase();
 
 LiquidCrystal_I2C lcd(DISPLAY_ADDR, DISPLAY_ROWS, DISPLAY_COLS);  // set the LCD address and the number of columns and rows
 
