6ab6e460f25c0abc90e1d197f206e57ae03501cb
djessup <djessup@users.noreply.github.com>
2024-07-13 10:30:14 +1000

    Update reflow_profile.h

--- include/reflow_profile.h
+++ include/reflow_profile.h
@@ -5,17 +5,17 @@
 #define HOTPLATE_RESCUE_REFLOW_PROFILE_H
 
 // Target temps in celsius
-#define PROFILE_SOAK_TEMP (125.00f)
-#define PROFILE_REFLOW_TEMP (180.00f)
+#define PROFILE_SOAK_TEMP (130.00f)
+#define PROFILE_REFLOW_TEMP (200.00f)
 #define PROFILE_COOLDOWN_TEMP (50.00f)
 
 // Time temp held once target is reached (in seconds)
-#define PROFILE_SOAK_DURATION (120) // 2 min
-#define PROFILE_REFLOW_DURATION (60) // 1 min
+#define PROFILE_SOAK_DURATION (150) // 2 min
+#define PROFILE_REFLOW_DURATION (90) // 1 min
 
 // Degrees per sec to ramp to the target temperature
 #define PROFILE_SOAK_RAMP_RATE (1.0f)
 #define PROFILE_REFLOW_RAMP_RATE (1.0f)
-#define PROFILE_COOLDOWN_RAMP_RATE (2.0f)
+#define PROFILE_COOLDOWN_RAMP_RATE (0.2f)
 
 #endif // HOTPLATE_RESCUE_REFLOW_PROFILE_H
